15dab9de18e552e0b7501a8b98ef6148df95518b
versatile-utility-4919 <jomariepolanco@gmail.com>
2020-07-17 18:34:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase) {
-    console.log("I can't hear you!");
+    return "I can't hear you!"
   } else if (string.toUpperCase) {
     return "YES INDEED"
   } else if (string === "I love you, Grandma.") {
